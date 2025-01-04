63987e3da033df9fee9b6a44e09cde9dbcbb2636
Theo <theoscartonlago@gmail.com>
2025-01-03 23:32:47 -0300

    Update index.js

---
+++ JavaScriptProjects/JS specifics/index.js
@@ -0,0 +1,25 @@
+//tenary operator:
+//shortcut for if and else statements
+
+//normal way:
+
+let age = 21;
+
+if(age >= 18) {
+
+    message = "You're an adult";
+
+} else {
+
+    message = "You're a minor";
+
+}
+
+//tenary way:
+
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+
+//let message is used because the string must be assigned a variable,
+// which in turn will be message.
+ 
+//works with boolean values etc
